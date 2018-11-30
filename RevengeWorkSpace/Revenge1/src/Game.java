2bcf22e0bb8745a82901c549a4a13946534a7022
Gabrial Zencha <ashungafac@gmail.com>
2018-11-30 15:22:09 +0300

    Initial commit

---
+++ RevengeWorkSpace/Revenge1/src/Game.java
@@ -0,0 +1,34 @@
+import java.awt.EventQueue;
+
+import views.UI;
+
+public class Game {
+
+	
+	//UI ui=new UI();
+	
+	public Game() {
+		
+		//ui.initComponents();
+		
+	}
+	/*public static void main(String[] args) {
+		
+		new Game();
+		
+	}
+*/
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UI frame = new UI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
